f51a54939d55523d0cc63f28cab9170eb3507be6
Somik Dhar <45973106+somikdhar729@users.noreply.github.com>
2025-12-01 22:50:57 +0530

    Add reduction kernels header file

---
+++ Reduction/kernels.h
@@ -0,0 +1,7 @@
+#pragma once
+__global__ void reduction_1(const float* input, float* output, int N);
+__global__ void reduction_2(const float* input, float* output, int N);
+__global__ void reduction_3(const float* input, float* output, int N);
+__global__ void reduction_4(const float* input, float* output, int N);
+__global__ void reduction_5(const float* input, float* output, int N);
+__global__ void reduction_6(const float* input, float* output, int N);
